7ed603f98f98b61161428719947d8c57909fa85c
Falk Schieber <12937991+pixelbucket-dev@users.noreply.github.com>
2021-02-13 17:57:47 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,65 @@
+// RxJS v6+
+import { fromEvent, merge, timer } from "rxjs";
+import {
+  map,
+  mapTo,
+  filter,
+  pluck,
+  switchMap,
+  takeWhile,
+  withLatestFrom
+} from "rxjs/operators";
+
+const durationInput = document.querySelector("#timer-input");
+const startButton = document.querySelector("#start-button");
+const resetButton = document.querySelector("#reset-button");
+const timeTextElement = document.querySelector("#time-text");
+
+// Capture input events
+const durationEvent$ = fromEvent(durationInput, "input").pipe(
+  map((e) => Number(e.target.value))
+);
+const enterEvent$ = fromEvent(durationInput, "keyup").pipe(
+  filter((e) => e.key === "Enter")
+);
+const startEvent$ = fromEvent(startButton, "click");
+const resetEvent$ = fromEvent(resetButton, "click").pipe(mapTo(-1));
+
+// Combine confirmation inputs
+const timerEvent$ = merge(enterEvent$, startEvent$);
+
+// Start timer from confirmation event
+const timerStateWithInput$ = timerEvent$.pipe(
+  withLatestFrom(durationEvent$),
+  pluck(1), // We're not interested in the timerEvent$ value, only the durationEvent$ one, pick second array value
+  filter((duration) => duration !== 0) // Ignore when 0 to keep innter Observable running
+);
+
+// Calculate remaining time
+const timer$ = merge(timerStateWithInput$, resetEvent$).pipe(
+  switchMap((duration) =>
+    merge(
+      timer(0, 1000).pipe(
+        map((r) => duration - r),
+        takeWhile((r) => r > 0, true) // Stop inner Observable when time is over
+      )
+    )
+  )
+);
+
+timer$.subscribe({
+  next: renderToDom
+});
+
+function renderToDom(time) {
+  if (time === -1) {
+    console.log("Reset");
+    durationInput.value = "";
+    timeTextElement.innerHTML = "Reset, please start again!";
+    return;
+  }
+
+  const text = time ? `Remaining time ${time} second(s).` : "Time is up!";
+  console.log(text);
+  timeTextElement.innerHTML = text;
+}
